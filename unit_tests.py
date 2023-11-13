6d99757acf8e3927745126ad8265cd2df2b58b38
223107111 <Alexandria.Guo@ge.com>
2023-11-13 17:46:32 -0500

    Updated unit tests, given changed types returned

--- unit_tests.py
+++ unit_tests.py
@@ -26,9 +26,9 @@ class stonesTestCase(unittest.TestCase):
 
         # seed(1)
         # random points, check all points are on circle
-        x3 = 0; y3 = 0; R3 = 5; n3 = 5
+        x3 = 0; y3 = 0; R3 = 5; n3 = 100
         xout3, yout3 = points(x3, y3, R3, n3, 2)
-        self.assertTrue(np.allclose(np.sqrt(np.square(xout3 - x3) + np.square(yout3 - y3))), R3)
+        self.assertTrue(np.allclose(np.sqrt(np.square(xout3 - x3) + np.square(yout3 - y3)), R3))
 
     def test_circumference(self):
 
@@ -36,25 +36,29 @@ class stonesTestCase(unittest.TestCase):
         x1 = np.array([0, -sqrt(2), 0, sqrt(2)])
         y1 = np.array([sqrt(2), 0, -sqrt(2), 0])
         out1 = np.array([2., 2., 2., 2.])
-        self.assertTrue(np.allclose(get_circumference(x1, y1, sqrt(2)), out1))
+        check, _1, _2 = get_circumference(x1, y1, sqrt(2))
+        self.assertTrue(np.allclose(check, out1))
 
         # non-centered square @ (1,1)
         x2 = np.array([1, 1-sqrt(2), 1, 1+sqrt(2)])
         y2 = np.array([1+sqrt(2), 1, 1-sqrt(2), 1])
         out2 = np.array([2., 2., 2., 2.])
-        self.assertTrue(np.allclose(get_circumference(x2, y2, sqrt(2)), out2))
+        check, _1, _2 = get_circumference(x2, y2, sqrt(2))
+        self.assertTrue(np.allclose(check, out2))
 
         # centered square, differently ordered pts
         x3 = np.array([0,  0, -sqrt(2), sqrt(2)])
         y3 = np.array([-sqrt(2), sqrt(2), 0, 0])
         out3 = np.array([2., 2., 2., 2.])
-        self.assertTrue(np.allclose(get_circumference(x3, y3, sqrt(2)), out3))
+        check, _1, _2 = get_circumference(x3, y3, sqrt(2))
+        self.assertTrue(np.allclose(check, out3))
 
         # unequally spaced pts, rectangle
         x4 = np.array([0.5, -0.5, -0.5, 0.5])
         y4 = np.array([sqrt(0.75), sqrt(0.75), -sqrt(0.75), -sqrt(0.75)])
         out4 = np.array([2*sqrt(0.75), 1., 2*sqrt(0.75), 1.]) # order manually checked
-        self.assertTrue(np.allclose(get_circumference(x4, y4, 1), out4))
+        check, _1, _2 = get_circumference(x4, y4, 1)
+        self.assertTrue(np.allclose(check, out4))
 
     def test_area(self):
 
@@ -62,19 +66,22 @@ class stonesTestCase(unittest.TestCase):
         in1 = np.array([2., 2., 2., 2.])
         R1 = sqrt(2)
         out1 = 4
-        self.assertTrue(np.allclose(Area_Calc(R1, in1), out1))
+        check, _ = Area_Calc(R1, in1)
+        self.assertTrue(np.allclose(check, out1))
 
         # pentagon
         in2 = np.array([1., 1., 1., 1., 1.])
         R2 = 0.75
         out2 = sqrt((0.75**2)-(0.5**2)) * 2.5
-        self.assertTrue(np.allclose(Area_Calc(R2, in2), out2))
+        check, _ = Area_Calc(R2, in2)
+        self.assertTrue(np.allclose(check, out2))
 
         # unequally spaced pts, rectangle
         in3 = np.array([2*sqrt(0.75), 1., 2*sqrt(0.75), 1.])
         R3 = 1.0
         out3 = 2*sqrt(0.75)
-        self.assertTrue(np.allclose(Area_Calc(R3, in3), out3))
+        check, _ = Area_Calc(R3, in3)
+        self.assertTrue(np.allclose(check, out3))
 
 
 
