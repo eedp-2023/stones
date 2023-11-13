6c33f1ea4e23bd4cff6ecabdbce6365933757857
223107111 <Alexandria.Guo@ge.com>
2023-11-12 23:15:00 -0500

    Corrected test_area unit test

--- unit_tests.py
+++ unit_tests.py
@@ -1,10 +1,12 @@
 import unittest
+import random # seed
+import numpy as np
 from math import sqrt
 
 from area import *
-from circumference import *
+from circumference import get_circumference
 from circum_plot import *
-from Estimated_and_Exact_Area import *
+from Estimated_and_Exact_Area import Area_Calc
 
 
 class stonesTestCase(unittest.TestCase):
@@ -30,9 +32,10 @@ class stonesTestCase(unittest.TestCase):
         self.assertTrue(np.allclose(get_circumference(x3, y3, sqrt(2)), out3))
 
         # unequally spaced pts, rectangle
-        x4 = np.array([5., -5., -5., 5.])
-        y4 = np.array([0.2, 0.2, -0.2, -0.2])
-        out4 = np.array([0.4, 10., 0.4, 10.]) # order manually checked
+        x4 = np.array([0.5, -0.5, -0.5, 0.5])
+        y4 = np.array([sqrt(0.75), sqrt(0.75), -sqrt(0.75), -sqrt(0.75)])
+        out4 = np.array([2*sqrt(0.75), 1., 2*sqrt(0.75), 1.]) # order manually checked
+        print(get_circumference(x4, y4, 1))
         self.assertTrue(np.allclose(get_circumference(x4, y4, 1), out4))
 
     def test_area(self):
@@ -50,9 +53,9 @@ class stonesTestCase(unittest.TestCase):
         self.assertTrue(np.allclose(Area_Calc(R2, in2), out2))
 
         # unequally spaced pts, rectangle
-        in3 = np.array([0.4, 10., 0.4, 10.])
+        in3 = np.array([2*sqrt(0.75), 1., 2*sqrt(0.75), 1.])
         R3 = 1.0
-        out3 = 20.8
+        out3 = 2*sqrt(0.75)
         self.assertTrue(np.allclose(Area_Calc(R3, in3), out3))
 
 
