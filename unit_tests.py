c49567f4646bffb090b8e086ab21b2aca85039cc
223107111 <Alexandria.Guo@ge.com>
2023-11-14 11:31:29 -0500

    Updated tests to match new inputs for get_circumference

--- unit_tests.py
+++ unit_tests.py
@@ -33,31 +33,35 @@ class stonesTestCase(unittest.TestCase):
     def test_circumference(self):
 
         # centered square
-        x1 = np.array([0, -sqrt(2), 0, sqrt(2)])
-        y1 = np.array([sqrt(2), 0, -sqrt(2), 0])
+        x1 = 0; y1 = 0; R1 = sqrt(2)
+        x_pts1 = np.array([0, -sqrt(2), 0, sqrt(2)])
+        y_pts1 = np.array([sqrt(2), 0, -sqrt(2), 0])
         out1 = np.array([2., 2., 2., 2.])
-        check, _1, _2 = get_circumference(x1, y1, sqrt(2))
+        check, _1, _2 = get_circumference(x_pts1, y_pts1, R1, x1, y1)
         self.assertTrue(np.allclose(check, out1))
 
-        # non-centered square @ (1,1)
-        x2 = np.array([1, 1-sqrt(2), 1, 1+sqrt(2)])
-        y2 = np.array([1+sqrt(2), 1, 1-sqrt(2), 1])
+        # non-centered square @ (3,4)
+        x2 = 3; y2 = 4; R2 = sqrt(2)
+        x_pts2 = np.array([x2, x2-sqrt(2), x2, x2+sqrt(2)])
+        y_pts2 = np.array([y2+sqrt(2), y2, y2-sqrt(2), y2])
         out2 = np.array([2., 2., 2., 2.])
-        check, _1, _2 = get_circumference(x2, y2, sqrt(2))
+        check, _1, _2 = get_circumference(x_pts2, y_pts2, R2, x2, y2)
         self.assertTrue(np.allclose(check, out2))
 
         # centered square, differently ordered pts
-        x3 = np.array([0,  0, -sqrt(2), sqrt(2)])
-        y3 = np.array([-sqrt(2), sqrt(2), 0, 0])
+        x3 = 0; y3 = 0; R3 = sqrt(2)
+        x_pts3 = np.array([0,  0, -sqrt(2), sqrt(2)])
+        y_pts3 = np.array([-sqrt(2), sqrt(2), 0, 0])
         out3 = np.array([2., 2., 2., 2.])
-        check, _1, _2 = get_circumference(x3, y3, sqrt(2))
+        check, _1, _2 = get_circumference(x_pts3, y_pts3, R3, x3, y3)
         self.assertTrue(np.allclose(check, out3))
 
         # unequally spaced pts, rectangle
-        x4 = np.array([0.5, -0.5, -0.5, 0.5])
-        y4 = np.array([sqrt(0.75), sqrt(0.75), -sqrt(0.75), -sqrt(0.75)])
+        x4 = 0; y4 = 0; R4 = 1
+        x_pts4 = np.array([0.5, -0.5, -0.5, 0.5])
+        y_pts4 = np.array([sqrt(0.75), sqrt(0.75), -sqrt(0.75), -sqrt(0.75)])
         out4 = np.array([2*sqrt(0.75), 1., 2*sqrt(0.75), 1.]) # order manually checked
-        check, _1, _2 = get_circumference(x4, y4, 1)
+        check, _1, _2 = get_circumference(x_pts4, y_pts4, R4, x4, y4)
         self.assertTrue(np.allclose(check, out4))
 
     def test_area(self):
